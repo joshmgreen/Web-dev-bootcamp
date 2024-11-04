e6bf9699aa732a970b09a8de8b617522f11ed25a
Josh Green <joshmg.codes@gmail.com>
2024-11-04 11:29:22 -0600

    Add complex state

---
+++ Changing Complex State/src/components/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {firstName} {lastName}
+      </h1>
+      <form>
+        <input
+          onChange={handleFirstNameChange}
+          name="fName"
+          placeholder="First Name"
+          value={firstName}
+        />
+        <input
+          onChange={handleLastNameChange}
+          name="lName"
+          placeholder="Last Name"
+          value={lastName}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
